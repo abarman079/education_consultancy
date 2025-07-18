aaabfdc421cf7804ffc2ac908e4d2e33a9ef946c
ab_arman <abarmanoffice@gmail.com>
2025-07-19 02:31:46 +0600

    configured netlify

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Chart } from 'chart.js/auto';
+
+import { Chart } from 'chart.js';
+
 
 const Dashboard = () => {
   const [stats, setStats] = useState({
